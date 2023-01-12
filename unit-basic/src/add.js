62fcaca0370c5bc1cbc016e53d1d5fc8473ba393
wslee94 <leemuo123@gmail.com>
2023-01-12 22:47:03 +0900

    계산기 테스트 코드 작성

--- unit-basic/src/add.js
+++ unit-basic/src/add.js
@@ -1,6 +1,5 @@
 function add(a, b) {
   return a + b;
-  expect(add(1, 2)).toBe(3);
 }
 
 module.exports = add;
---
+++ unit-basic/src/test/calculator.test.js
@@ -0,0 +1,59 @@
+const Calculator = require("../calculator.js");
+const calculator = new Calculator();
+
+// 각각의 테스트는 독립적으로 만드는게 중요하다!
+
+describe("Calculator", () => {
+  let cal;
+
+  // 테스트 수행 전 실행
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets", () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+
+  it("clear", () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("adds", () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it("multiplies", () => {
+    cal.set(5);
+    cal.multiply(4);
+    expect(cal.value).toBe(20);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
